130f8e4225a2cd549b53c079fe030fdf7bdf1564
Belal Sukari <belalesukari@gmail.com>
2021-02-10 14:27:09 +0100

    pu

--- server.js
+++ server.js
@@ -143,6 +143,6 @@ app.post('/bloger',(req,res)=>{
  app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
 });
 
